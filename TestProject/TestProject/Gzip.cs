c9cc600c59df555746373adbbf47245f73b1444f
Andrey.Fokin <axel_1989@mail.ru>
2016-10-20 09:36:16 +0300

    New class

---
+++ TestProject/TestProject/Gzip.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: prog2
+ * Date: 20.10.2016
+ * Time: 9:34
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace TestProject
+{
+	/// <summary>
+	/// Description of Gzip.
+	/// </summary>
+	public class Gzip
+	{
+		public Gzip()
+		{
+		}
+	}
+}
