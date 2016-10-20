af9edf5cbb57d64f3960781e6267cc1b63c13554
Andrey.Fokin <axel_1989@mail.ru>
2016-10-20 09:28:15 +0300

    new

---
+++ TestProject/TestProject/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: prog2
+ * Date: 20.10.2016
+ * Time: 9:22
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace TestProject
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
